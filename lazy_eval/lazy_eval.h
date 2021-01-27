d5ac945a0fefae72e60c360ab066c5fa2085f681
Pedro <30598480+TricksterByte@users.noreply.github.com>
2021-01-27 22:30:48 +0000

    add lazy_eval header

---
+++ lazy_eval/lazy_eval.h
@@ -0,0 +1,30 @@
+#include <mutex>
+#include <type_traits>
+
+template <typename F>
+class lazy_eval
+{
+private:
+	F m_computation;
+	mutable decltype(m_computation()) m_cache;
+	mutable std::once_flag m_value_flag;
+
+public:
+	template<typename U>
+	lazy_eval(U&& computation)
+	: m_computation(std::forward<U>(computation))
+	{
+		// empty
+	}
+
+	operator const decltype(m_computation())& () const
+	{
+		std::call_once(m_value_flag, [this] {
+			m_cache = m_computation();
+		});
+
+		return m_cache;
+	}
+};
+
+template<typename F> lazy_eval(F) -> lazy_eval<F>;
